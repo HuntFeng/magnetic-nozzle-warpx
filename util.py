cecfab7d92519852bb749fbd36abef200f27a140
Hunt Feng <huntfeng@nia-login07.scinet.local>
2023-11-20 18:39:54 -0500

    able to estimate total time using previous runs

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import re
 
 # constants
 constants = sp.constants
@@ -55,3 +56,45 @@ def debye_length(T: float, n: float):
     n0: particle number density
     """
     return 7430 * np.sqrt(T / n)
+
+
+"""Postprocessing functions"""
+
+
+def extract_data(filename):
+    """
+    Extarct the time steps from a standard warpx output log
+
+    each column corresponds to the 6 numbers in the output log
+    time_date[:,0]: STEP
+    time_date[:,1]: TIME
+    time_date[:,2]: DT
+    time_date[:,3]: Evolve time
+    time_date[:,4]: This step
+    time_date[:,5]: Avg. per step
+    """
+    regex_core = re.compile(r"MPI initialized with ([0-9]*) MPI processes")
+    regex_omp = re.compile(r"OMP initialized with ([0-9]*) OMP threads")
+    regex_step = re.compile(
+        r"STEP [0-9]* ends.*\n.* Avg\. per step = ([0-9]*[.])?[0-9]+ s", re.MULTILINE
+    )
+
+    string_data = []
+    specs = {}
+
+    print("Processing " + filename + " ...", end="")
+    with open(filename) as f:
+        text = f.read()
+        specs["cores"] = int(regex_core.search(text).group(1))
+        specs["omp_threads"] = int(regex_omp.search(text).group(1))
+        string_data = [s.group(0) for s in regex_step.finditer(text)]
+
+    regex_real = re.compile(r" -?[\d.]+(?:e-?\d+)?", re.MULTILINE)
+
+    time_data = np.zeros([len(string_data), 6])
+    for i, ss in enumerate(string_data):
+        numbers = regex_real.findall(ss)
+        time_data[i, :] = np.array(numbers)
+
+    print("...done!")
+    return specs, time_data
